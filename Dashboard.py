f46ea2d64809424a1aa989ef4f18437b57fb1370
vicho-bece <vicentebecerra72@gmail.com>
2026-08-04 16:40:03 -0400

    Agregar 4 gráficos
    Se agregar 4 graficos sobre:
    - Porcentaje de géneros musicales
    - Cantidad de artistas que debutaron en su año
    - Porcentaje de idioma en la música
    - Porcentaje de participación de artistas por su género

--- Dashboard.py
+++ Dashboard.py
@@ -27,7 +27,7 @@ with col2:
     fst_genre = fn.most_genre(data=data)
 
     genre = fst_genre['Primary Genre'].iloc[0]
-    genre_prcnt = fst_genre['percent'].iloc[0]
+    genre_prcnt = fst_genre['Percent'].iloc[0]
 
     st.text(f"Género más escuchado: {genre}")
     st.metric(label="Porcentaje:", value=genre_prcnt*100, format="%f%%")
@@ -36,9 +36,24 @@ with col3:
     dt_country = fn.most_country(data=data)
 
     country = dt_country['Country of Origin'].iloc[0]
-    country_prcnt = dt_country['percent'].iloc[0]
+    country_prcnt = dt_country['Percent'].iloc[0]
 
     st.text(f"País de orígen más escuchado: {country}")
     st.metric(label="Porcentaje:", value=country_prcnt*100, format="%f%%")
 
 
+graph1, graph2 = st.columns(2)
+
+with graph1:
+    fn.graph_data(data, 0)
+
+with graph2:
+    fn.graph_data(data, 1)
+
+graph3, graph4 = st.columns(2)
+
+with graph3:
+    fn.graph_data(data, 2)
+
+with graph4:
+    fn.graph_data(data, 3)
--- Functions.py
+++ Functions.py
@@ -1,25 +1,71 @@
 import pandas as pd
+import streamlit as st
+import plotly.express as px
 
 def most_genre(data:pd.DataFrame) -> pd.DataFrame:
 
-    generos = data.groupby(['Primary Genre']).size().reset_index(name='counts')
-    generos = generos.sort_values(by="counts", ascending=False)
+    generos = data.groupby(['Primary Genre']).size().reset_index(name='Quantity')
+    generos = generos.sort_values(by="Quantity", ascending=False)
 
-    total = generos['counts'].sum()
+    total = generos['Quantity'].sum()
 
-    for i in generos.iterrows():
-        generos['percent'] = generos['counts'] / total
+    generos['Percent'] = generos['Quantity'] / total
 
     return generos
 
 def most_country(data:pd.DataFrame) -> pd.DataFrame:
 
-    country = data.groupby(['Country of Origin']).size().reset_index(name='counts')
-    country = country.sort_values(by="counts", ascending=False)
+    country = data.groupby(['Country of Origin']).size().reset_index(name='Quantity')
+    country = country.sort_values(by="Quantity", ascending=False)
 
-    total = country['counts'].sum()
+    total = country['Quantity'].sum()
 
-    for i in country.iterrows():
-        country['percent'] = country['counts'] / total
+    country['Percent'] = country['Quantity'] / total
 
     return country
+
+def graph_data(data:pd.DataFrame, option):
+
+    match option:
+        case 0: 
+            datos = most_genre(data)
+            title = "Porcentajes de los generós escuchados en 2025"
+            labelx = 'Primary Genre'
+            labely = 'Percent'
+
+        case 1:
+            datos = data.groupby(['Debut Year']).size().reset_index(name='Quantity')
+            datos = datos.sort_values(by="Quantity", ascending=False)
+
+            title = "Número de artistas que consiguieron su debut según el año"
+            labelx = 'Debut Year'
+            labely = 'Quantity'
+
+        case 2:
+            datos = data.groupby(['Primary Language']).size().reset_index(name='Quantity')
+            datos = datos.sort_values(by="Quantity", ascending=False)
+
+            total = datos['Quantity'].sum()
+
+            datos['Percent'] = datos['Quantity'] / total
+
+            title = "Idioma más escuchado en 2025"
+            labelx = 'Primary Language'
+            labely = 'Percent'
+
+        case 3:
+            datos = data.groupby(['Sex']).size().reset_index(name='Quantity')
+            datos = datos.sort_values(by="Quantity", ascending=False)
+
+            total = datos['Quantity'].sum()
+
+            datos['Percent'] = datos['Quantity'] / total
+
+            title = "Género de artistas más escuchado en 2025"
+            labelx = 'Sex'
+            labely = 'Percent'
+
+
+    st.text(title)
+    fig = px.bar(datos, x=labelx, y=labely)
+    st.plotly_chart(fig, width='stretch', config = {'scrollZoom': False, 'zoom': False, 'displayModeBar':False})
